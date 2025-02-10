8464884ea60cf06c9277c8056e7e815688ac7f3b
howisthepie <sexxxpistols14@gmail.com>
2025-02-10 21:08:05 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,21 +1,19 @@
-import telebot
-import os
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
 
-# Берём токен из переменных среды (Railway)
-TOKEN = os.getenv("TOKEN")
+TOKEN =8084921352:AAE7iK_Wwk_O836lAG0I6c_eH_bptO2_QAQ
 
-# Создаём бота
-bot = telebot.TeleBot(TOKEN)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-# Команда /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я бот для мониторинга состояния ребёнка!")
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Привет! Я твой бот.")
 
-# Обработчик любых сообщений
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "Я пока умею только отвечать :)")
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling()
 
-# Запуск бота
-bot.polling()
+if __name__ == "__main__":
+    asyncio.run(main())
